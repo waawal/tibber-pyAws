c8b9e642307ae323786ee0a927fb193c1d4c75e7
Daniel Høyer Iversen <mail@dahoiv.net>
2020-03-05 08:03:52 +0100

    aiobotocore (#3)
    * aiobotocore
    
    * requirements
    
    * style

--- tibber_aws/s3.py
+++ tibber_aws/s3.py
@@ -1,7 +1,7 @@
 import datetime
 import logging
 
-import boto3
+import aiobotocore
 import botocore
 
 _LOGGER = logging.getLogger(__name__)
@@ -14,35 +14,29 @@ STATE_PRECONDITION_FAILED = "precondition_failed"
 class S3Bucket:
     def __init__(self, bucket_name, region_name='eu-west-1'):
         self._bucket_name = bucket_name
-        self._region_name = region_name
-        self.s3 = None
-        self._bucket = None
+        session = aiobotocore.get_session()
+        self.client = session.create_client('s3', region_name=region_name)
 
-    def get_bucket(self):
-        self.s3 = boto3.resource(
-            's3',
-            region_name=self._region_name,
-        )
-        try:
-            self._bucket = self.s3.create_bucket(Bucket=self._bucket_name,
-                                                 CreateBucketConfiguration={'LocationConstraint':
-                                                                            self._region_name}
-                                                 )
-        except self.s3.meta.client.exceptions.BucketAlreadyOwnedByYou:
-            self._bucket = self.s3.Bucket(self._bucket_name)
-
-    def load_json(self, key, if_unmodified_since=None):
+    async def load_data(self, key, if_unmodified_since=None):
         if if_unmodified_since is None:
             if_unmodified_since = datetime.datetime(1900, 1, 1)
         try:
-            res = self._bucket.Object(key=key).get(IfUnmodifiedSince=if_unmodified_since)["Body"].read()
-            return res.decode('utf-8'), STATE_OK
-        except self.s3.meta.client.exceptions.NoSuchKey:
+            res = await (await self.client.get_object(Bucket=self._bucket_name,
+                                                      Key=key,
+                                                      IfUnmodifiedSince=if_unmodified_since))["Body"].read()
+        except self.client.exceptions.NoSuchKey:
             return None, STATE_NOT_EXISTING
         except botocore.exceptions.ClientError as exp:
             if "PreconditionFailed" in str(exp):
                 return None, STATE_PRECONDITION_FAILED
             raise
+        return res.decode('utf-8'), STATE_OK
 
-    def store_json(self, key, data):
-        self._bucket.Object(key=key).put(Body=data)
+    async def store_data(self, key, data):
+        resp = await self.client.put_object(Bucket=self._bucket_name,
+                                            Key=key,
+                                            Body=data)
+        return resp
+
+    async def close(self):
+        await self.client.close()
