5d094c962322412f572ca9cd8fe83e343c232da1
Daniel Høyer Iversen <mail@dahoiv.net>
2020-04-03 09:48:58 +0200

    0.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='tibber_aws',
     packages=['tibber_aws'],
     install_requires=['boto3', 'aiobotocore==0.12.0' 'aiohttp', 'async_timeout', 'numpy'],
-    version='0.2.0',
+    version='0.2.1',
     description='A python3 library to communicate with Aws',
     python_requires='>=3.5.3',
     author='Tibber',
