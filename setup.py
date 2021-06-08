745144a5d879f5ccd689f844caee0039b2c5adbb
Daniel Hjelseth Høyer <github@dahoiv.net>
2021-06-08 10:54:06 +0200

    python 3.6
    Signed-off-by: Daniel Hjelseth Høyer <github@dahoiv.net>

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
 from setuptools import setup
 
 setup(
-    name='tibber_aws',
-    packages=['tibber_aws'],
-    install_requires=['aiobotocore', 'aiohttp', 'async_timeout', 'boto3', 'botocore', 'numpy', 'async_exit_stack'],
-    version='0.4.0',
-    description='A python3 library to communicate with Aws',
-    python_requires='>=3.5.3',
-    author='Tibber',
-    author_email='hello@tibber.com',
-    url='https://github.com/tibbercom/tibber-pyAws',
+    name="tibber_aws",
+    packages=["tibber_aws"],
+    install_requires=[
+        "aiobotocore",
+        "aiohttp",
+        "async_timeout",
+        "boto3",
+        "botocore",
+        "numpy",
+        'async_exit_stack; python_version < "3.7.0"',
+    ],
+    version="0.4.0",
+    description="A python3 library to communicate with Aws",
+    python_requires=">=3.5.3",
+    author="Tibber",
+    author_email="hello@tibber.com",
+    url="https://github.com/tibbercom/tibber-pyAws",
     license="MIT",
     classifiers=[
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-        ]
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
